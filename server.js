9c9bdbac2db12b518a8525e3302890dc110c78a8
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-22 15:56:47 +0200

    create get /pet endpoint

--- server.js
+++ server.js
@@ -31,21 +31,47 @@ const app = express();
 app.use(express.json());
 app.use(express.static("pet-images"));
 
+app.get("/pet", (req, res) => {
+  if (req.url === "/pet") {
+    res.json(pets);
+  } else {
+    const type = req.query.type;
+    const height = req.query.height;
+    const weight = req.query.weight;
+    const name = req.query.name;
+    const adoptionStatus = req.query.adoptionStatus;
+    let petsSearchResults = pets.filter((pet) => {
+      return (
+        (pet.type === type || type === "Species") &&
+        (pet.adoptionStatus === adoptionStatus ||
+          adoptionStatus === "Adoption status") &&
+        ((pet.height >= parseInt(height.split("-")[0]) &&
+          pet.height <= parseInt(height.split("-")[1])) ||
+          height === "Height(cm)") &&
+        ((pet.weight >= parseInt(weight.split("-")[0]) &&
+          pet.weight <= parseInt(weight.split("-")[1])) ||
+          weight === "Weight(kg)") &&
+        (pet.name === name || !name)
+      );
+    });
+    res.json(petsSearchResults);
+  }
+});
 app.get("/pet/:id", (req, res) => {
   const { id } = req.params;
   res.json(petsObj[id]);
 });
 
-app.post("/signup", (req, res) => {});
-app.post("/photos", upload.single("picture"), (req, res, next) => {
-  let newPicObj = {
-    image: req.file.filename,
-    caption: req.body.caption,
-  };
-  images.push(newPicObj);
-  fs.writeFileSync("./images.json", JSON.stringify(images, null, 2));
-  res.redirect("/gallery.html");
-});
+// app.post("/signup", (req, res) => {});
+// app.post("/photos", upload.single("picture"), (req, res, next) => {
+//   let newPicObj = {
+//     image: req.file.filename,
+//     caption: req.body.caption,
+//   };
+//   images.push(newPicObj);
+//   fs.writeFileSync("./images.json", JSON.stringify(images, null, 2));
+//   res.redirect("/gallery.html");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
