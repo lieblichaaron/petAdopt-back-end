c45d6e45f36a0d3ba4ffbcdb3ed4fc769a6eab9c
Aaron Lieblich <lieblichaaron@gmail.com>
2020-12-22 14:49:25 +0200

    add mock json data

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static("pet-images"));
 
-app.post("/signup", (req, res) => {});
 app.get("/pet/:id", (req, res) => {
   const { id } = req.params;
   res.json(petsObj[id]);
 });
 
+app.post("/signup", (req, res) => {});
 app.post("/photos", upload.single("picture"), (req, res, next) => {
   let newPicObj = {
     image: req.file.filename,
